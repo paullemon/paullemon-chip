9fd9195192e5a8cca9443050f9217ef279625db8
plemon <paul@expansiagroup.com>
2020-05-21 10:54:57 -0500

    fixed quote on variable files

--- modules/regionpeering/variables.tf
+++ modules/regionpeering/variables.tf
@@ -1,11 +1,11 @@
 #Variables inputted from the main module
 variable "default_tags" { type = map }
 variable "region" { type = list }
-variable "vpc-tfe0 {}
-variable "vpc-tfe1 {}
-variable "vpc-app0 {}
-variable "vpc-app1 {}
-variable "vpc-app2 {}
-variable "vpc-adm0 {}
-variable "vpc-adm1 {}
-variable "vpc-adm2 {}
+variable "vpc-tfe0" {}
+variable "vpc-tfe1" {}
+variable "vpc-app0" {}
+variable "vpc-app1" {}
+variable "vpc-app2" {}
+variable "vpc-adm0" {}
+variable "vpc-adm1" {}
+variable "vpc-adm2" {}
